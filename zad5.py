a0171a0c6dc09f328ae1012ae79def40ab3818f0
edytaroz <93035594+edytaroz@users.noreply.github.com>
2022-05-30 16:21:49 +0200

    Add files via upload

---
+++ zad5.py
@@ -0,0 +1,34 @@
+from zad5testy import runtests
+from queue import PriorityQueue
+#Edyta Rozczypała
+#Deklarujemy kolejke priorytetową. Wkładamy do niej te indeksy i ich wartości,
+#kóre są w naszym zasięgu i których wartości nie są równe 0. Wtedy zdejmujemy
+#największą wartość z kolejki i dodajemy ją do zasięgu. Powtarzamy aż zasięg
+#będzie większy od tablicy albo przejdziemy przez całą tablicę. Sortujemy wynikową
+#tablicę. Złożoność pamięciowa to O(n) a czasowa O(n log n )
+
+
+def pq_frog(T):
+    n =len(T)
+    lilie = PriorityQueue()
+    skok = T[0]
+    l = [0]
+    for i in range(1,n):
+        if skok < n - 1:
+            if T[i] != 0:
+                lilie.put((-T[i],i))
+            if i == skok:
+                maks = lilie.get()[1]
+                l.append(maks)
+                skok += T[maks]
+    l.sort()
+    return l
+
+
+def plan(T):
+    # tu prosze wpisac wlasna implementacje
+    return pq_frog(T)
+
+
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( plan, all_tests = True )
